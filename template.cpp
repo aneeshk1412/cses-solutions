ee0f0e62cb84f0b70510139720149872fdd363af
Aneesh Shetty <170040022iitbkai@gmail.com>
2021-06-22 16:13:32 +0530

    updates gitignore and adds template

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#pragma GCC optimize ("-O3")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Typedefs
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+// Constants
+const ll MOD = 1000000007;
+const ld PI = 3.141592653589793;
+const ld EPS = 1e-9;
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
